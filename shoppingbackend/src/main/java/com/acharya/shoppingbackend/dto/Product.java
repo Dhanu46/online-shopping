823e5e290d638734e29a4bbd8d41da7b8f8e993c
Dhanu46 <dhanu.r46@gmail.com>
2019-12-10 07:37:13 -0800

    Id..

--- shoppingbackend/src/main/java/com/acharya/shoppingbackend/dto/Product.java
+++ shoppingbackend/src/main/java/com/acharya/shoppingbackend/dto/Product.java
@@ -4,11 +4,14 @@ import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+
 
 @Entity
 public class Product {
 
 	// private fields
+	@Id
 	private int id;
 	private String code;
 	private String name;
