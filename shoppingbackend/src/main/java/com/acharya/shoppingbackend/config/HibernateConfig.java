9473110832600d0f89c9954bea6b985b499c2f28
Dhanu46 <dhanu.r46@gmail.com>
2019-12-10 01:16:19 -0800

    updated successfully CategoryTestCase

--- shoppingbackend/src/main/java/com/acharya/shoppingbackend/config/HibernateConfig.java
+++ shoppingbackend/src/main/java/com/acharya/shoppingbackend/config/HibernateConfig.java
@@ -1,14 +1,16 @@
 package com.acharya.shoppingbackend.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -16,42 +18,62 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class HibernateConfig {
 
-	private static StandardServiceRegistry standardServiceRegistry;
-	  private static SessionFactory sessionFactory;
-
-	  static{
-		    if (sessionFactory == null) {
-		      try {
-		        // Create StandardServiceRegistry
-		        standardServiceRegistry = new StandardServiceRegistryBuilder()
-		            .configure()
-		            .build();
-		        // Create MetadataSources
-		        MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
-		        // Create Metadata
-		        Metadata metadata = metadataSources.getMetadataBuilder().build();
-		        // Create SessionFactory
-		        sessionFactory = metadata.getSessionFactoryBuilder().build();
-		      } catch (Exception e) {
-		        e.printStackTrace();
-		        if (standardServiceRegistry != null) {
-		          StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
-		        }
-		      }
-		    }
-	  }
-	  //Utility method to return SessionFactory
-	  public static SessionFactory getSessionFactory() {
-		  return sessionFactory;
-	  }
-	  
-	  @Bean
-		public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-
-			HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
-			return transactionManager;
-		}
-
-	  
-	}
 	
+
+	// change the below based on the DBMS you need
+	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost/~/onlineshopping";
+	private final static String DATABASE_DRIVER = "org.h2.Driver";
+	private final static String DATABASE_DIALECT = "org.hibernate.dialect.H2Dialect";
+	private final static String DATABASE_USERNAME = "sa";
+	private final static String DATABASE_PASSWORD = "";
+
+	@Bean
+	public DataSource getDataSource() {
+
+		BasicDataSource datasource = new BasicDataSource();
+
+		datasource.setDriverClassName(DATABASE_DRIVER);
+		datasource.setUrl(DATABASE_URL);
+		datasource.setUsername(DATABASE_USERNAME);
+		datasource.setPassword(DATABASE_PASSWORD);
+
+		return datasource;
+
+	}
+
+	@Bean
+	public SessionFactory getSessionFactory(DataSource datasource) {
+
+		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(datasource);
+
+		builder.addProperties(getHibernateProperties());
+		builder.scanPackages("com.acharya.shoppingbackend.dto");
+
+		return builder.buildSessionFactory();
+		
+	}
+
+	// ALL THE HIBERNATE PROPERTIES WRITTEN IN THIS
+
+	
+	
+	private Properties getHibernateProperties() {
+
+		Properties properties = new Properties();
+
+		properties.put("hibernate.dialect", DATABASE_DIALECT);
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+
+		return properties;
+	}
+
+	// transcation managment bean
+	@Bean
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+		return transactionManager;
+	}
+
+}
